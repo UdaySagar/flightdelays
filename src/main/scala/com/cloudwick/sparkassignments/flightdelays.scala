ebc8e9268a23fdfff935277e2385bfe4452b4c79
UdaySagar <uday.sagar.panjala@gmail.com>
2017-03-10 00:28:11 -0800

    code to read csv file

--- src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
+++ src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
@@ -1,5 +1,8 @@
 package com.cloudwick.sparkassignments
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 /**
  * Hello world!
  *
@@ -7,5 +10,33 @@ package com.cloudwick.sparkassignments
 object flightdelays {
   def main(args: Array[String]): Unit = {
     println("Project to calculate flight delays")
+    val conf = new SparkConf().setAppName("wordcount")
+    val sc = new SparkContext(conf)
+    val csvInPath = "/path/to/csv/abc.csv"
+    val df = sc.read.format("com.databricks.spark.csv").option("header","true").load(csvInPath)
   }
 }
+
+package com.cloudwick.scalaassignments
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+/**
+  * Hello world!
+  *
+  */
+object wordcount {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("wordcount")
+    val sc = new SparkContext(conf)
+    val file = sc.textFile(args(0))
+    val counts = file.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    counts.saveAsTextFile(args(1))
+
+  }
+}
+

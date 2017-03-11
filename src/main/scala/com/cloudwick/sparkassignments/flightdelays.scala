31402e3ca3f38c0c3fbc76bdd07fd4ce5cecb65b
UdaySagar <uday.sagar.panjala@gmail.com>
2017-03-11 12:09:11 -0800

    build main dataframe

--- src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
+++ src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
@@ -1,42 +1,28 @@
 package com.cloudwick.sparkassignments
 
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import com.databricks._
 /**
  * Hello world!
  *
  */
 object flightdelays {
   def main(args: Array[String]): Unit = {
-    println("Project to calculate flight delays")
-    val conf = new SparkConf().setAppName("wordcount")
+    val conf = new SparkConf().setAppName("flightdelays")
     val sc = new SparkContext(conf)
-    val csvInPath = "/path/to/csv/abc.csv"
-    val df = sc.read.format("com.databricks.spark.csv").option("header","true").load(csvInPath)
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("cars.csv")
+    val df1 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2006.csv")
+    val df2 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2007.csv")
+    val df3 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2008.csv")
+    val maindf = df.unionAll(df1).unionAll(df2).unionAll(df3)
+    val selectedData = df.select("year", "model")
+    selectedData.write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .save("newcars.csv")
   }
 }
-
-package com.cloudwick.scalaassignments
-
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-
-/**
-  * Hello world!
-  *
-  */
-object wordcount {
-
-  def main(args: Array[String]): Unit = {
-
-    val conf = new SparkConf().setAppName("wordcount")
-    val sc = new SparkContext(conf)
-    val file = sc.textFile(args(0))
-    val counts = file.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
-    counts.saveAsTextFile(args(1))
-
-  }
-}
-

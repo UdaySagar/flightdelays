670ef8e8d271a369182d953bda439725680fb35c
UdaySagar <uday.sagar.panjala@gmail.com>
2017-03-13 17:26:25 -0700

    weekly reports dataframe version 1

--- src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
+++ src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import com.databricks._
+
 /**
  * Hello world!
  *
@@ -19,13 +20,9 @@ object flightdelays {
     val df2 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2007.csv")
     val df3 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2008.csv")
     val maindf = df.unionAll(df1).unionAll(df2).unionAll(df3)
-    val sortedDf = maindf.orderBy("Year", "Month", "DayofMonth")
-    val result = maindf.select($"Year", $"Month", $"DayofMonth", $"UniqueCarrier", $"ArrDelay", $"DepDelay").groupBy($"Year", $"Month", $"DayofMonth", $"UniqueCarrier").agg(countDistinct($"ArrDelay") as "distinct_arrival_delay", countDistinct($"DepDelay") as "distinct_departure_delay")
-    //result.show(5)
-    val selectedData = df.select("year", "model")
-    selectedData.write
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .save("newcars.csv")
+
+    val weekofYearDf = maindf.withColumn("weekofYear", weekofyear(concat($"Year", lit("-"), $"Month", lit("-"), $"DayofMonth")))
+    val weeklyReports = weekofYearDf.select("Year", "Month", "DayofMonth", "DayofWeek", "UniqueCarrier", "ArrDelay", "DepDelay", "weekofYear").groupBy("Year", "weekofYear", "UniqueCarrier").agg(sum("ArrDelay")).orderBy("Year", "weekofYear", "UniqueCarrier")
+
   }
 }

d863e213d01d7faa10eae1e910c146480534e094
UdaySagar <uday.sagar.panjala@gmail.com>
2017-03-15 23:08:34 -0700

    adding arrdelay and depdelay

--- src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
+++ src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
@@ -22,8 +22,18 @@ object flightdelays {
     val maindf = df.unionAll(df1).unionAll(df2).unionAll(df3)
 
     val weekofYearDf = maindf.withColumn("weekofYear", weekofyear(concat($"Year", lit("-"), $"Month", lit("-"), $"DayofMonth")))
-    val totalDelayDf = maindf.withColumn("totalDelay", $"ArrDelay" + $"DepDelay")
-    val totalDelayDf1 = totalDelayDf.filter("Origin = 'SFO' or Dest ='")
+    //val totalDelayDf = maindf.withColumn("totalDelay", $"ArrDelay" + $"DepDelay")
+    //val totalDelayDf1 = totalDelayDf.filter("Origin = 'SFO' or Dest ='")
+    val sfoOriginDf = weekofYearDf.filter("Origin = 'SFO'")
+    val sfoDestDf = weekofYearDf.filter("Dest = 'SFO'")
+
+    val sfoOriginDelayDf = sfoOriginDf.withColumn("apDelay", $"DepDelay")
+    val sfoDestDelayDf = sfoDestDf.withColumn("apDelay", $"ArrDelay")
+
+    val delayDf = sfoOriginDelayDf.unionAll(sfoDestDelayDf)
+    val sfoDelayDf = delayDf.groupBy("Year", "weekofYear", "UniqueCarrier").agg(sum("apDelay")).orderBy("Year", "weekofYear", "UniqueCarrier")
+
+
     val weeklyReports = weekofYearDf.select("Year", "Month", "DayofMonth", "DayofWeek", "UniqueCarrier", "ArrDelay", "DepDelay", "weekofYear", "Origin", "Dest").groupBy("Year", "weekofYear", "Origin").agg(sum("DepDelay")).orderBy("Year", "weekofYear", "Origin")
 
   }

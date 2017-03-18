4beaa86f9de16b157350752cbb57f86d741683c8
UdaySagar <uday.sagar.panjala@gmail.com>
2017-03-18 14:20:54 -0700

    Joined airports.csv dataframe to existing dataframes to filter based on city

--- src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
+++ src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
@@ -26,21 +26,32 @@ object flightdelays {
     val df1 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2006.csv")
     val df2 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2007.csv")
     val df3 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2008.csv")
+
+    val airportsDf = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/airports.csv")
+    val carriersDf = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/carriers.csv")
+
     val maindf = df.unionAll(df1).unionAll(df2).unionAll(df3)
 
     val weekofYearDf = maindf.withColumn("weekofYear", weekofyear(concat(col("Year"), lit("-"), col("Month"), lit("-"), col("DayofMonth"))))
-    //val totalDelayDf = maindf.withColumn("totalDelay", $"ArrDelay" + $"DepDelay")
 
-    val sfoOriginDf = weekofYearDf.filter(s"Origin = '${airportName}'")
-    val sfoDestDf = weekofYearDf.filter(s"Dest = '${airportName}'")
 
-    val sfoOriginDelayDf = sfoOriginDf.withColumn("apDelay", col("DepDelay"))
-    val sfoDestDelayDf = sfoDestDf.withColumn("apDelay", col("ArrDelay"))
+    val givenCityAirportsDf = airportsDf.filter(s"city = '${cityName}'")
 
-    val delayDf = sfoOriginDelayDf.unionAll(sfoDestDelayDf)
+    val givenCityOriginDf = weekofYearDf.join(givenCityAirportsDf, col("Origin") === col("iata"), "left")
+    val givenCityDestDf = weekofYearDf.join(givenCityAirportsDf, col("Dest") === col("iata"), "left")
 
-    val sfoDelayDf = delayDf.where("apDelay > 0").groupBy("Year", "weekofYear", "UniqueCarrier").agg(sum("apDelay")).orderBy("Year", "weekofYear", "UniqueCarrier")
+    //val sfoOriginDf = weekofYearDf.filter(s"Origin = '${airportName}'")
+    //val sfoDestDf = weekofYearDf.filter(s"Dest = '${airportName}'")
 
-    sfoOriginDf.save(args(0))
+    val originDelayDf = givenCityOriginDf.withColumn("apDelay", col("DepDelay"))
+    val destDelayDf = givenCityDestDf.withColumn("apDelay", col("ArrDelay"))
+
+    val delayDf = originDelayDf.unionAll(destDelayDf)
+
+    val groupedDelayDf = delayDf.where("apDelay > 0").groupBy("Year", "weekofYear", "UniqueCarrier").agg(sum("apDelay")).orderBy("Year", "weekofYear", "UniqueCarrier")
+
+    val carrierNamesDf = groupedDelayDf.join(carriersDf, col("Code") === col("UniqueCarrier"))
+
+    carrierNamesDf.save(args(0))
   }
 }

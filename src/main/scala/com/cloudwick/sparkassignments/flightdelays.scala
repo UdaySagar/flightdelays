a6fad9169cd11bdb0bcc62bb5bb1eb422b82b718
UdaySagar <uday.sagar.panjala@gmail.com>
2017-03-18 13:13:42 -0700

    Added type safe config library - version 1.30

--- src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
+++ src/main/scala/com/cloudwick/sparkassignments/flightdelays.scala
@@ -4,7 +4,9 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import com.databricks._
+import com.databricks.spark.csv
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.functions._
 
 /**
  * Hello world!
@@ -15,27 +17,30 @@ object flightdelays {
     val conf = new SparkConf().setAppName("flightdelays")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
+
+    val confFile = ConfigFactory.load()
+    val cityName = confFile.getString("flightsdelay.cityName")
+    val airportName = confFile.getString("flightsdelay.airportName")
+
     val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2005.csv")
     val df1 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2006.csv")
     val df2 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2007.csv")
     val df3 = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs://10.2.0.81:8020/sparkassignment/2008.csv")
     val maindf = df.unionAll(df1).unionAll(df2).unionAll(df3)
 
-    val weekofYearDf = maindf.withColumn("weekofYear", weekofyear(concat($"Year", lit("-"), $"Month", lit("-"), $"DayofMonth")))
+    val weekofYearDf = maindf.withColumn("weekofYear", weekofyear(concat(col("Year"), lit("-"), col("Month"), lit("-"), col("DayofMonth"))))
     //val totalDelayDf = maindf.withColumn("totalDelay", $"ArrDelay" + $"DepDelay")
-    //val totalDelayDf1 = totalDelayDf.filter("Origin = 'SFO' or Dest ='")
-    val sfoOriginDf = weekofYearDf.filter("Origin = 'SFO'")
-    val sfoDestDf = weekofYearDf.filter("Dest = 'SFO'")
 
-    val sfoOriginDelayDf = sfoOriginDf.withColumn("apDelay", $"DepDelay")
-    val sfoDestDelayDf = sfoDestDf.withColumn("apDelay", $"ArrDelay")
+    val sfoOriginDf = weekofYearDf.filter(s"Origin = '${airportName}'")
+    val sfoDestDf = weekofYearDf.filter(s"Dest = '${airportName}'")
+
+    val sfoOriginDelayDf = sfoOriginDf.withColumn("apDelay", col("DepDelay"))
+    val sfoDestDelayDf = sfoDestDf.withColumn("apDelay", col("ArrDelay"))
 
     val delayDf = sfoOriginDelayDf.unionAll(sfoDestDelayDf)
 
     val sfoDelayDf = delayDf.where("apDelay > 0").groupBy("Year", "weekofYear", "UniqueCarrier").agg(sum("apDelay")).orderBy("Year", "weekofYear", "UniqueCarrier")
 
-
-    //val weeklyReports = weekofYearDf.select("Year", "Month", "DayofMonth", "DayofWeek", "UniqueCarrier", "ArrDelay", "DepDelay", "weekofYear", "Origin", "Dest").groupBy("Year", "weekofYear", "Origin").agg(sum("DepDelay")).orderBy("Year", "weekofYear", "Origin")
-
+    sfoOriginDf.save(args(0))
   }
 }
